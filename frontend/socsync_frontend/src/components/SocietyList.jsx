d8132f46d630ce75401e35e24875c47c94bd9cb7
rSamaei <rezsamaei@gmail.com>
2025-03-01 12:44:02 +0000

    Add SocietyList component for displaying societies

---
+++ frontend/socsync_frontend/src/components/SocietyList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function SocietyList() {
+  return (
+    <div className="society-list">
+      <h2>Societies</h2>
+      <div className="societies-container">
+        {/* Add society items here */}
+        <p>No societies to display</p>
+      </div>
+    </div>
+  );
+}
+
+export default SocietyList;
